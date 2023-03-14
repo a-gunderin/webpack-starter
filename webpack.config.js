ed0d430cdad91286cd161292a42b13c7bb8ac99a
Andrei Gunderin <ga@web-notes.info>
2023-03-14 14:16:42 +0200

    build: add images

--- webpack.config.js
+++ webpack.config.js
@@ -37,7 +37,7 @@ const config = {
 				filename: 'js/[name].js?v=[contenthash:8]',
 			},
 			css: {
-				filename: 'css/[name].css?v[contenthash:8]',
+				filename: 'css/[name].css?v=[contenthash:8]',
 			},
 		}),
 		atomizer,
@@ -45,15 +45,34 @@ const config = {
 
 	module: {
 		rules: [
+
 			{
 				test: /\.pug$/,
 				loader: PugPlugin.loader,
 			},
+
 			{
 				test: /\.(css|sass|scss)$/,
 				use: ['css-loader', 'sass-loader'],
 			},
+
+			{
+				test: /\.(png|jpe?g|webp|gif)$/i,
+				type: 'asset/resource',
+				generator: {
+					filename: 'images/[name].[hash:8][ext]',
+				},
+			},
+
+			{
+				test: /\.svg$/i,
+				type: 'asset/resource',
+				generator: {
+					filename: 'svg/[name].[hash:8][ext]',
+				},
+			}
 		],
+
 	},
 
 	devServer: {
